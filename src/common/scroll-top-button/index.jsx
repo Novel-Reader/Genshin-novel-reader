4905c03fc0aa93e7dea4a96e86e2ba246500782c
Michael An <2331806369@qq.com>
2023-01-28 11:47:46 +0800

    0.3 frontend scroll top maimeng

---
+++ src/common/scroll-top-button/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { UncontrolledTooltip } from 'reactstrap';
+import './index.css';
+
+export default class ScrollTopIcon extends Component {
+  render() {
+    const id = 'novel-scroll-top-icon';
+    return (
+      <>
+        <div className="scroll-top-icon" onClick={this.props.onClick} style={{bottom: this.props.bottom}} id={id}></div>
+        <UncontrolledTooltip
+          placement='right'
+          target={id}
+          fade={false}
+          delay={{show: 0, hide: 0}}
+        >
+          {'回到顶部'}
+        </UncontrolledTooltip>
+      </>
+    )
+  }
+}
--- src/main/long-page/index.jsx
+++ src/main/long-page/index.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { parseTxtToHTML } from '../../utils';
+import ScrollTopIcon from '../../common/scroll-top-button';
 
 import './index.css';
 
@@ -11,6 +12,48 @@ export default class LongPage extends Component {
     style: PropTypes.object,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMoving: false,
+      isShowTopIcon: false,
+    };
+    this.timer = null;
+  }
+
+  onScroll = (e) => {
+    if (!this.state.isMoving && !this.state.isShowTopIcon && e.target.scrollTop > window.innerHeight) {
+      this.setState({
+        isShowTopIcon: true,
+      });
+    }
+    if (!this.state.isMoving && this.state.isShowTopIcon && e.target.scrollTop < window.innerHeight) {
+      this.setState({
+        isShowTopIcon: false,
+      });
+    }
+  }
+
+  scrollToTop = () => {
+    this.setState({
+      isMoving: true,
+    }, () => {
+      let currentTop = this.longPageRef.scrollTop;
+      this.timer = setInterval(() => {
+        currentTop = currentTop - 100;
+        this.longPageRef.scrollTop = currentTop;
+        if (currentTop < 0) {
+          clearInterval(this.timer);
+          this.timer = null;
+          this.setState({
+            isMoving: false,
+            isShowTopIcon: false,
+          });
+        }
+      }, 20);
+    });
+  }
+
   render() {
     const { currentFile, style = {} } = this.props;
     const list = parseTxtToHTML(currentFile);
@@ -18,14 +61,16 @@ export default class LongPage extends Component {
     const pageStyle = {
       backgroundImage: `url('${style.backgroundImage || defaultImage}')`,
     };
-    // 此时 style 背景色需要变成 0.75 半透明效果比较好
     return (
-      <div className="long-page" style={pageStyle}>
+      <div className="long-page" style={pageStyle} onScroll={this.onScroll} ref={node => this.longPageRef = node}>
         <div className='long-page-container' style={Object.assign({}, style, {opacity: 0.75})}>
           {list.map((item, index) => {
             return (<p key={index}>{item}</p>);
           })}
         </div>
+        {
+          <ScrollTopIcon onClick={this.scrollToTop} bottom={this.state.isShowTopIcon ? 20 : -70}/>
+        }
       </div>
     )
   }
