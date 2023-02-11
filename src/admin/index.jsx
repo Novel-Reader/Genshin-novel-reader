45fd52bbe32c5ac6e93bf6fd4ac7f1139137022c
Michael An <2331806369@qq.com>
2023-02-11 14:43:09 +0800

    0.6 frontend react-router and error page

---
+++ src/admin/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './index.css';
+
+// https://reactrouter.com/en/main/start/tutorial
+
+export default class Admin extends Component {
+  render() {
+    return (
+      <div className="admin d-flex center">
+        <h1>Admin Page</h1>
+        <Link to="/">Not support yet, click to return novel reader.</Link>
+      </div>
+    );
+  }
+}
