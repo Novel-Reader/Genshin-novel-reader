bb683021470b4a70b3bf7611ec91fbf5c37918f9
Michael An <2331806369@qq.com>
2023-02-28 10:00:05 +0800

    0.7 mobile not support yet

---
+++ src/App-mobile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css/App-mobile.css';
+
+export default function AppMobile() {
+  return (
+    <div className="mobile-not-support">暂不支持移动端小说阅读器，请使用 PC 端</div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MediaQuery from 'react-responsive';
 import App from "./App";
+import AppMobile from './App-mobile';
 import ErrorPage from "./error-page";
 import Admin from "./admin";
 
@@ -12,7 +14,16 @@ import "./css/common.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: (
+      <>
+        <MediaQuery query="(min-device-width: 767.8px)">
+          <App/>
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 767.8px)">
+          <AppMobile/>
+        </MediaQuery>
+      </>
+    ),
     errorElement: <ErrorPage />,
   },
   {
@@ -24,5 +35,7 @@ const router = createBrowserRouter([
 // ReactDOM.render is no longer supported in React 18. 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const container = document.getElementById('root');
+
 const root = createRoot(container);
+
 root.render(<RouterProvider router={router} />);
